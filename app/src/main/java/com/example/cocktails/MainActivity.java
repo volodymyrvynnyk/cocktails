8e0b45e117c73a5d91d5894e466fef5e23e8c583
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2020-04-30 13:06:41 +0300

    Main Activity View

--- app/src/main/java/com/example/cocktails/MainActivity.java
+++ app/src/main/java/com/example/cocktails/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.cocktails;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView historyText;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        historyText = findViewById(R.id.tv_history);
+    }
+
+    public void onClickSearch(View view) {
+        Context context = MainActivity.this;
+
+        Class destinationActivity= SearchActivity.class;
+
+        Intent childActivityIntent = new Intent(context, destinationActivity);
+        startActivity(childActivityIntent);
     }
 }
